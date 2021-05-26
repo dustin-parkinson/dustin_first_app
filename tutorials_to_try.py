d3c1eaef5fd0f26c312ecf27e8e698e78b546c89
Dustin Parkinson <dustin@streamlit.com>
2021-05-26 12:36:12 -0600

    change again

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -2,20 +2,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+import streamlit as st
 
+left_column, right_column = st.beta_columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
-
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
