59624eda445e9c6732cc602c5d4fc496c1303d6a
Dustin Parkinson <dustin@streamlit.com>
2021-05-26 12:32:49 -0600

    added another dataframe

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
