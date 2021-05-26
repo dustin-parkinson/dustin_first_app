e8d9de867b38541dcfbee4c22c19730691695cb0
Dustin Parkinson <dustin@streamlit.com>
2021-05-26 14:33:33 -0600

    magic command

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -16,3 +16,18 @@ data2 = np.random.randn(10, 2)
 
 # Append the new data to the existing chart.
 chart.add_rows(data2)
+
+
+
+# Draw a title and some text to the app:
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
+df = pd.DataFrame({'col1': [1,2,3]})
+df  # <-- Draw the dataframe
+
+x = 10
+'x', x  # <-- Draw the string 'x' and then the value of x
