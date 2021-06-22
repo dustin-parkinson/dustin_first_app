540e16d9d5724479473d1f3059e92de25ee638f2
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 13:30:11 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -4,8 +4,10 @@ import time
 
 st.title('Check out this sweet Demo!')
 
+Streamlit
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png)
 
+         
 st.image(
 'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
 
