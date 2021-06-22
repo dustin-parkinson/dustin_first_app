8fe201b29a642c3fb3774186581abb5a83b47ff7
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 13:28:33 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -2,39 +2,8 @@ import streamlit as st
 import numpy as np
 import time
 
+st.title('Check out this sweet Demo!')
+
 st.image(
 'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
-
-'''
-
-# Get some data.
-data = np.random.randn(10, 2)
-
-# Show the data as a chart.
-chart = st.line_chart(data)
-
-# Wait 1 second, so the change is clearer.
-time.sleep(1)
-
-# Grab some more data.
-data2 = np.random.randn(10, 2)
-
-# Append the new data to the existing chart.
-chart.add_rows(data2)
-
-st.title("Hi I'm Dustin")
-
-# Draw a title and some text to the app:
-'''
-# This is the document title
-
-'''
-
-df = pd.DataFrame({'col1': [1,2,3]})
-df  # <-- Draw the dataframe
-
-x = 10
-'x', x  # <-- Draw the string 'x' and then the value of x
-
-'''
 
