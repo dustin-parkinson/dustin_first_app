2798fa0ce0a46c5e9a8b4f09f76e67d4654d6639
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 14:50:33 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 st.title('Streamlit+KeenFootware')
 
-st.text('This is just some text')
+st.text('This is to show you easy it is to change text')
 
+st.header('This is the Streamlit Logo')
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
 st.empty()
-         
+st.header('This is the Keens Footware Logo')         
 st.image(
 'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
 
