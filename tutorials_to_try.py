dc181767d797122f6165c312a82b3c3756a1c041
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 15:36:08 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -4,11 +4,11 @@ st.title('Streamlit+KeenFootware')
 
 st.text('This is to show you easy it is to change text')
 
-st.header('This is the Streamlit Logo. And it looks super cool')
+st.header('This is the Streamlit Logo.')
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
 st.empty()
-st.header('This is the Keen Footware Logo, And it looks even cooler')         
+st.header('This is the FIGS Logo')         
 st.image(
-'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
+'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-exp3.licdn.com%2Fdms%2Fimage%2FC560BAQFcqTOZQKmZ6Q%2Fcompany-logo_200_200%2F0%2F1519904088642%3Fe%3D2159024400%26v%3Dbeta%26t%3DhHMnxq88JbvpzF5F5ynrAH8fxzyWHQaf_3ts-9cP-_c&imgrefurl=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fwearfigs&tbnid=P_GBVw4o619a0M&vet=12ahUKEwj-mLuKmazxAhWU6KwKHUQnDrUQMygCegUIARDLAQ..i&docid=v6f5ynOwag9e0M&w=200&h=200&q=figs%20logo&ved=2ahUKEwj-mLuKmazxAhWU6KwKHUQnDrUQMygCegUIARDLAQ')
 
