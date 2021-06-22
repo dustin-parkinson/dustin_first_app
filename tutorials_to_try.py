ef3c393fabfc172afea2a4f29fb1f7b45f1ed0a0
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 14:48:19 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import numpy as np
-import time
 
-st.title('Check out this sweet Demo!')
+st.title('Streamlit+KeenFootware')
 
+st.text('This is just some text'
 
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
+st.empty()
          
 st.image(
 'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
