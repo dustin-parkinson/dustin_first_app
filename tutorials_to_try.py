5fbbd447b4f9595ff3f438adcb319bf5de63442c
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 15:15:39 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -4,11 +4,11 @@ st.title('Streamlit+KeenFootware')
 
 st.text('This is to show you easy it is to change text')
 
-st.header('This is the Streamlit Logo')
+st.header('This is the Streamlit Logo. And it looks super cool')
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
 st.empty()
-st.header('This is the Keen Footware Logo')         
+st.header('This is the Keen Footware Logo, And it looks even cooler')         
 st.image(
 'https://www.keenfootwear.com/on/demandware.static/-/Library-Sites-keen_shared/default/dwba71f1df/meta/keenlogofb.png')
 
