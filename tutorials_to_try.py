1da1848d7ff858261be0b543a649b7552e7f320a
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-22 16:40:15 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -4,11 +4,11 @@ st.title('Streamlit+KeenFootware')
 
 st.text('This is to show you easy it is to change text')
 
-st.header('This is the Streamlit Logo.')
+st.header('This is the Streamlit Logo. it is kinda cool')
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
 st.empty()
-st.header('This is the FIGS Logo')         
+st.header('This is the FIGS Logo. it is even cool!!!')         
 st.image(
 'https://www.criteo.com/wp-content/uploads/2020/01/ss-logo-FIGS-min.png')
 
