d3beee05bde21670340af401c9cc38c2f61bd3c4
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-06-30 16:30:27 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Streamlit+KeenFootware')
+st.title('Streamlit+Aisle')
 
 st.text('This is to show you easy it is to change text')
 
@@ -8,6 +8,7 @@ st.header('This is the Streamlit Logo. it is kinda cool')
 st.image('https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png')
 
 st.empty()
-st.header('This is a great logo!')         
+st.header('This is a great logo!')
+st.header('But there is not a logo here, what the heck?')
 
 
