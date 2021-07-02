c0b7a7c5d31ee739f6e3f751ee92605b82c76901
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-07-02 09:12:38 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -11,6 +11,6 @@ st.empty()
 st.header('Dustin writes an app!')
 st.header('But is it a good app?')
 
-st.image(https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-exp1.licdn.com%2Fdms%2Fimage%2FC4D0BAQHGZwbCPc4G1g%2Fcompany-logo_200_200%2F0%2F1587503509793%3Fe%3D2159024400%26v%3Dbeta%26t%3DAGuEdTcrpbgiCK6U3epi7Z6KGopQDkT9fk7edHkoIu4&imgrefurl=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fcafemedia&tbnid=vqvuvk-u6v30RM&vet=12ahUKEwjg3dng1cTxAhXEMs0KHXu6D4oQMygCegUIARCfAQ..i&docid=Ohtztmv5b3xMKM&w=200&h=200&itg=1&q=cafe%20media%20logo&ved=2ahUKEwjg3dng1cTxAhXEMs0KHXu6D4oQMygCegUIARCfAQ)
+st.image('https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-exp1.licdn.com%2Fdms%2Fimage%2FC4D0BAQHGZwbCPc4G1g%2Fcompany-logo_200_200%2F0%2F1587503509793%3Fe%3D2159024400%26v%3Dbeta%26t%3DAGuEdTcrpbgiCK6U3epi7Z6KGopQDkT9fk7edHkoIu4&imgrefurl=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fcafemedia&tbnid=vqvuvk-u6v30RM&vet=12ahUKEwjg3dng1cTxAhXEMs0KHXu6D4oQMygCegUIARCfAQ..i&docid=Ohtztmv5b3xMKM&w=200&h=200&itg=1&q=cafe%20media%20logo&ved=2ahUKEwjg3dng1cTxAhXEMs0KHXu6D4oQMygCegUIARCfAQ')
 
 
