e72f6f9482f2d491f9115f93305fb32ad1feb8e0
dustin-parkinson <84750425+dustin-parkinson@users.noreply.github.com>
2021-07-02 09:15:54 -0600

    Update tutorials_to_try.py

--- tutorials_to_try.py
+++ tutorials_to_try.py
@@ -11,6 +11,6 @@ st.empty()
 st.header('Dustin writes an app!')
 st.header('But is it a good app?')
 
-st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrustx.org%2Fpubs%2Fcafemedia-onboard-logo%2F&psig=AOvVaw2aN1e5qhe25ZA_Jddp1huB&ust=1625325205290000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCJi61bDWxPECFQAAAAAdAAAAABAI')
+
 
 
